877796c95cffc4d0b939571844e604f06f5e8bb7
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-06-06 21:31:18 +0200

    seeds

---
+++ src/seeds/owner.seed.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name": "Carlos",
+        "surname": "Pacheco",
+        "properties" : [],
+        "image": ""
+    },
+    {
+        "name": "Jon",
+        "surname": "Rahm",
+        "properties" : [],
+        "image": ""
+    }
+]
---
+++ src/seeds/property.seed.js
@@ -0,0 +1,24 @@
+[
+    {
+    "name": "Building Sequoia",
+    "constructionYear": 2005,
+    "owners" : [],
+    "geometry": {
+      "location": {
+        "lat": 47.361019,
+        "lng": 8.446365
+        }
+        }
+    },
+    {
+    "name": "Apartments Onyx",
+    "constructionYear": 2011,
+    "owners" : [],
+    "geometry": {
+      "location": {
+        "lat": 47.370145,
+        "lng": 8.489695
+        }
+        }
+    },
+]
